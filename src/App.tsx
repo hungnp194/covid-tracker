592b4a5f7a23ece6ef4c763420b6995c17330cb0
hungnp194 <61738572+hungnp194@users.noreply.github.com>
2021-09-28 20:06:46 +0700

    Add HighShadowButton (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layout from './theme/layout';
+import { HighShadowButton } from './components/Buttons';
 
 function App() {
   return (
@@ -11,13 +12,17 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+        <div style={{ width: 260 }}>
+          <HighShadowButton fullWidth>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer">
+              Learn React
+            </a>
+          </HighShadowButton>
+        </div>
       </header>
     </Layout>
   );
